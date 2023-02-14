12cd53bc62bb98863d67bec79be60bb138804ab2
vishnuvijayankurup <aruzaryan@gmail.com>
2023-02-14 04:37:25 -0500

    your commit comment

--- bash/sysinfo.sh
+++ bash/sysinfo.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
-echo "system information"
-echo "hostname:"
-hostname -f
-cat /etc/*-release
-echo "ip address:"
- hostname -I
-df / 
+
+# FQDN and hostname
+FQDN=$(hostname -f)
+HOSTNAME=$(hostname)
+# IP address
+IP=$(ip route get 1.1.1.1 | awk '{print $7}')
+# OS NAME 
+OS=$(lsb_release -d | awk -F'\t' '{print $2}')
+#space on the disk
+SPACE=$(df -h / | awk '{print $4}' | sed -n 2p)
+#TEMPLATE OUTPUT
+TEMPLATE=$(cat << EOF
+Report for $HOSTNAME
+===============
+FQDN: $FQDN
+OPERATING SYSTEM VERSION AND NAME: $OS
+IP ADDRESS: $IP
+ROOT FILESYSTEM FREE SPACE: $SPACE
+===============
+EOF
+)
+#OUTPUT
+echo -e "\n$TEMPLATE"
 
